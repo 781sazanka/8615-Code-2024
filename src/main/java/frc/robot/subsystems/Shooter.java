c2d294f257df89495c673eb5e18f42b118d30d8c
eb1ta1 <ebitaiume@gmail.com>
2024-02-24 23:40:27 +0900

    add shooter test code

---
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class Shooter extends SubsystemBase {
+
+  final TalonFX motor1 = new TalonFX(Constants.Shooter.topFalconMotorCanId, "rio");
+  final TalonFX motor2 = new TalonFX(Constants.Shooter.bottomFalconMotorCanId, "rio");
+  CANSparkMax SparkMaxMotor = new CANSparkMax(Constants.Shooter.sparkMaxCanId, MotorType.kBrushless);
+
+  public Shooter() {
+    SparkMaxMotor.restoreFactoryDefaults();
+  }
+
+  public void shoot(double output) {
+    motor1.setControl(new DutyCycleOut(output));
+    motor2.setControl(new DutyCycleOut(output));
+  }
+
+  public void feed(double output) {
+    SparkMaxMotor.set(output);
+  }
+
+  @Override
+  public void periodic() {
+    motor1.stopMotor();
+    motor2.stopMotor();
+    SparkMaxMotor.stopMotor();
+  }
+}
